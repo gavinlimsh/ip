d33046ffcb1b73207eac4e3f9a088cd3ba44f2f2
gavin <gavinlimsh@gmail.com>
2026-08-19 18:39:35 +0800

    Update GigaBot.java

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# GigaBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/GigaBot.java
@@ -0,0 +1,22 @@
+public class GigaBot {
+    public static void main(String[] args) {
+        String banner = "   _______             ____        _    \n"
+                      + "  / _____             |  _ \\      | |   \n"
+                      + " | |  __ _  __ _  __ _| |_) | ___ | |_  \n"
+                      + " | | |_ | |/ _` |/ _` |  _ < / _ \\| __| \n"
+                      + " | |__| | | (_| | (_| | |_) | (_) | |_  \n"
+                      + "  \\_____|_|\\__, |\\__,_|____/ \\___/ \\__| \n"
+                      + "            __/ |                       \n"
+                      + "           |___/                        \n";
+
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        System.out.println(banner);
+        System.out.println("Hello! I'm GigaBot!");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
