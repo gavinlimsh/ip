0fd0b9d5960eb4f045d5048d857698b29918a469
gavin <gavinlimsh@gmail.com>
2026-08-28 19:00:45 +0800

    Added echo function to Bot

--- src/main/java/GigaBot.java
+++ src/main/java/GigaBot.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class GigaBot {
     public static void main(String[] args) {
         String banner = "   _____ _             ____        _    \n"
@@ -16,7 +18,26 @@ public class GigaBot {
         System.out.println("Hello! I'm GigaBot!");
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+
+        // Initialize Scanner
+        Scanner in = new Scanner(System.in);
+
+        // Listen in on new input
+        while (true) {
+            String userInput = in.nextLine();
+
+            // End conversation condition
+            if (userInput.equals("bye")) {
+                System.out.println(horizontalLine);
+                System.out.println("[GigaBot] >> Shutting down. Hope to see you again soon!");
+                System.out.println(horizontalLine);
+                break;
+            }
+
+            System.out.println(horizontalLine);
+            System.out.println("[GigaBot] >> " + userInput);
+            System.out.println(horizontalLine);
+        }
+        in.close();
     }
 }
