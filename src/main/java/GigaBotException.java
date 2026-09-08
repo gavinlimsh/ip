5846df08b9704e70b9bd655cc8c8a0f2ab2dcfbf
gavin <gavinlimsh@gmail.com>
2026-09-08 14:20:41 +0800

    feat: Error handling

---
+++ src/main/java/GigaBotException.java
@@ -0,0 +1,8 @@
+/**
+ * Represents exceptions specific to the GigaBot application.
+ */
+public class GigaBotException extends Exception {
+    public GigaBotException(String message) {
+        super(message);
+    }
+}
